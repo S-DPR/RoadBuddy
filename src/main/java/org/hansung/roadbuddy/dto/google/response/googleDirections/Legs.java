c907c0d0f484b0920bce2e1bc86cd8706d1f759a
S-DPR <thankyouformuch@naver.com>
2024-04-08 00:40:24 +0900

    feat: GoogleDirections와 TMapDirections DTO 추가

---
+++ src/main/java/org/hansung/roadbuddy/dto/google/response/googleDirections/Legs.java
@@ -0,0 +1,38 @@
+package org.hansung.roadbuddy.dto.google.response.googleDirections;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class Legs {
+    private LegsInfo arrival_time;
+    private LegsInfo departure_time;
+    private Distance distance;
+    private Duration duration;
+    private String end_address;
+    private RoutesCoordinate end_location;
+    private String start_address;
+    private RoutesCoordinate start_location;
+    private List<Steps> steps;
+
+    public void setTotalDistance() {
+        if (steps == null || steps.isEmpty()) return;
+        distance.setValueAndText(steps.stream().mapToLong(i -> {
+            i.updateDistance();
+            return i.getDistance().getValue();
+        }).sum());
+    }
+
+    public void setTotalTime() {
+        if (steps == null || steps.isEmpty()) return;
+        duration.setValueAndText(steps.stream().mapToLong(i -> {
+            i.updateDuration();
+            return i.getDuration().getValue();
+        }).sum());
+    }
+}
---
+++ src/main/java/org/hansung/roadbuddy/dto/google/response/googleDirections/Polyline.java
@@ -0,0 +1,12 @@
+package org.hansung.roadbuddy.dto.google.response.googleDirections;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Polyline {
+    private String points;
+}
