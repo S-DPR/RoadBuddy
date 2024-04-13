28f5714cdce7ccfe150a40550a9716103f114599
S-DPR <thankyouformuch@naver.com>
2024-04-13 18:15:52 +0900

    feat: Geomeetry - coordinates 앞/뒤 가져올 때 비어있으면 null 반환

--- src/main/java/org/hansung/roadbuddy/dto/tmap/response/tmapDirections/Geometry.java
+++ src/main/java/org/hansung/roadbuddy/dto/tmap/response/tmapDirections/Geometry.java
@@ -1,16 +1,12 @@
 package org.hansung.roadbuddy.dto.tmap.response.tmapDirections;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.google.maps.internal.PolylineEncoding;
-import com.google.maps.model.LatLng;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.hansung.roadbuddy.deserialization.TMapCoordinatesDeserializer;
 import org.hansung.roadbuddy.dto.Coordinate;
-import org.hansung.roadbuddy.dto.google.response.googleDirections.Polyline;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Setter
@@ -22,11 +18,13 @@ public class Geometry {
     List<List<Double>> coordinates;
 
     public Coordinate getFirstCoordinate() {
+        if (coordinates.isEmpty()) return null;
         List<Double> coord = coordinates.get(0);
         return Coordinate.listToCoordinate(coord);
     }
 
     public Coordinate getLastCoordinate() {
+        if (coordinates.isEmpty()) return null;
         List<Double> coord = coordinates.get(coordinates.size()-1);
         return Coordinate.listToCoordinate(coord);
     }
