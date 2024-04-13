9d4d6037a1e1b78b8afa52d4b228d440d4ea49ee
S-DPR <thankyouformuch@naver.com>
2024-04-13 18:16:58 +0900

    delete: 미사용 코드 제거

--- src/main/java/org/hansung/roadbuddy/dto/tmap/response/tmapDirections/Properties.java
+++ src/main/java/org/hansung/roadbuddy/dto/tmap/response/tmapDirections/Properties.java
@@ -23,16 +23,4 @@ public class Properties {
     private String facilityName;
     private Long turnType;
     private String pointType;
-
-    public String timeToString() {
-        long min = (totalTime + 59) / 60;
-        long hour = min / 60;
-        min = min % 60;
-
-        if (hour > 0) {
-            return String.format("%d시간\n%d분", hour, min);
-        } else {
-            return String.format("%d분", min);
-        }
-    }
 }
