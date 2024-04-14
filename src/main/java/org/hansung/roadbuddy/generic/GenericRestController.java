d2a284f0058bb74f928069dd3f0ec1d47143d347
S-DPR <thankyouformuch@naver.com>
2024-04-14 19:40:58 +0900

    feat: (임시) data 직접 넣어서 보내도록 수정

--- src/main/java/org/hansung/roadbuddy/generic/GenericRestController.java
+++ src/main/java/org/hansung/roadbuddy/generic/GenericRestController.java
@@ -8,16 +8,12 @@ import java.util.Map;
 import java.util.function.Function;
 
 public abstract class GenericRestController {
-    protected <T> Map wrapServiceResponse(T reqDto, Function<T, Map> serviceMethod) {
-        Map ret = new HashMap();
-        ret.put("data", serviceMethod.apply(reqDto));
-        return ret;
-    }
-
-    protected ResponseEntity toResponse(Object ret) {
-        if (ret == null) {
+    protected ResponseEntity toResponse(Object data) {
+        if (data == null) {
             return new ResponseEntity(HttpStatus.NO_CONTENT);
         }
+        Map ret = new HashMap();
+        ret.put("data", data);
         return new ResponseEntity(ret, HttpStatus.OK);
     }
 }
