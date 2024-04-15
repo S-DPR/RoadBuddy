3313f19b1dba7ee482c188994c257146222daeaf
S-DPR <thankyouformuch@naver.com>
2024-04-15 22:38:53 +0900

    feat: Directions 보내줄 dto

---
+++ src/main/java/org/hansung/roadbuddy/dto/roadBuddy/response/DirectionsResDto.java
@@ -0,0 +1,6 @@
+package org.hansung.roadbuddy.dto.roadBuddy.response;
+
+import org.hansung.roadbuddy.dto.google.response.googleDirections.GoogleDirectionResDto;
+
+public class DirectionsResDto extends GoogleDirectionResDto {
+}
