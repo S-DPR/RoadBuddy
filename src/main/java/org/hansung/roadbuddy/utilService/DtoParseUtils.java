51e5437fa96a8f5abb3e60bd92ffd6cb5f6948b1
S-DPR <thankyouformuch@naver.com>
2024-04-15 22:39:21 +0900

    fix: DtoParseUtils는 Service가 아님

--- src/main/java/org/hansung/roadbuddy/utilService/DtoParseUtils.java
+++ src/main/java/org/hansung/roadbuddy/utilService/DtoParseUtils.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
 public class DtoParseUtils {
     public static List<LatLng> extractLatLngInGeometry(Geometry geometry) {
         List<LatLng> coords = new ArrayList<>();
@@ -53,29 +52,47 @@ public class DtoParseUtils {
 
     public static List<Steps> featuresToSteps(List<Features> features) {
         List<Steps> res = new ArrayList<>();
-        // 하나는 Point, 하나는 LineString이라서. 둘이 세트임.
-        // 그런데 마지막은 도착 Point 하나라서 -1.
-        for (int i = 0; i < features.size()-1; i += 2) {
-            Features point = features.get(i);
-            Features lineString = features.get(i+1);
-            Properties pointProperties = point.getProperties();
-            Properties lineStringProperties = lineString.getProperties();
-            List<LatLng> latlngs = DtoParseUtils.extractLatLngInGeometry(lineString.getGeometry());
-
-            Distance distance = new Distance(lineStringProperties.getDistance());
-            Duration duration = new Duration(lineStringProperties.getTime());
-
-            res.add(Steps.builder()
-                    .distance(distance)
-                    .duration(duration)
-                    .travel_mode("WALKING")
-                    .maneuver(pointProperties.getTurnType().toString())
-                    .html_instructions(pointProperties.getDescription())
-                    .polyline(new Polyline(PolylineEncoding.encode(latlngs)))
-                    .end_location(RoutesCoordinate.coordinateToRoutesCoordinate(lineString.getGeometry().getLastCoordinate()))
-                    .start_location(RoutesCoordinate.coordinateToRoutesCoordinate(lineString.getGeometry().getFirstCoordinate()))
-                    .build());
+        // point 뒤에 linestring이 있으면 point는 linestring을 수식하지만,
+        // linestring 단독으로 나오면 turnType이 없어서 공백으로 채움
+        // 그런데 마지막은 도착 Point 하나. 마지막으로 처리하기 좋게 되어있음.
+        Features point = null;
+        for (int idx = 0; idx < features.size(); idx++) {
+            Features cur = features.get(idx);
+            if (cur.getGeometry().getType().equals("Point")) {
+                point = cur;
+                continue;
+            }
+            Features lineString = features.get(idx);
+            res.add(pointAndLineStringToSteps(point, lineString));
+            point = null;
         }
         return res;
+    }
+
+    public static Steps pointAndLineStringToSteps(Features point, Features lineString) {
+        Properties lineStringProperties = lineString.getProperties();
+        String turnType, description;
+        if (point == null) { // linestring에 대한 설명이 없음
+            turnType = "";
+            description = lineStringProperties.getDescription();
+        } else {
+            turnType = point.getProperties().getTurnType().toString();
+            description = point.getProperties().getDescription();
+        }
+        List<LatLng> latlngs = DtoParseUtils.extractLatLngInGeometry(lineString.getGeometry());
+
+        Distance distance = new Distance(lineStringProperties.getDistance());
+        Duration duration = new Duration(lineStringProperties.getTime());
+
+        return Steps.builder()
+                .distance(distance)
+                .duration(duration)
+                .travel_mode("WALKING")
+                .maneuver(turnType)
+                .html_instructions(description)
+                .polyline(new Polyline(PolylineEncoding.encode(latlngs)))
+                .end_location(RoutesCoordinate.coordinateToRoutesCoordinate(lineString.getGeometry().getLastCoordinate()))
+                .start_location(RoutesCoordinate.coordinateToRoutesCoordinate(lineString.getGeometry().getFirstCoordinate()))
+                .build();
     }
 }
