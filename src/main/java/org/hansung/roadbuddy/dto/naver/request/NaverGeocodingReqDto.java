b3f3e1c69bae3f5d48a5738964de83bb27645e78
S-DPR <thankyouformuch@naver.com>
2024-04-19 22:40:51 +0900

    feat: Naver Service 추가

---
+++ src/main/java/org/hansung/roadbuddy/dto/naver/request/NaverGeocodingReqDto.java
@@ -0,0 +1,26 @@
+package org.hansung.roadbuddy.dto.naver.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hansung.roadbuddy.dto.Coordinate;
+import org.hansung.roadbuddy.generic.GenericRequestDTO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+public class NaverGeocodingReqDto extends GenericRequestDTO {
+    private String query;
+    private Coordinate coordinate = null;
+
+    @Override
+    public Map<String, String> toMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("query", query);
+        if (coordinate != null) {
+            map.put("coordinate", coordinate.getLongitude() + "," + coordinate.getLatitude());
+        }
+        return map;
+    }
+}
---
+++ src/main/java/org/hansung/roadbuddy/service/NaverOpenAPIService.java
@@ -0,0 +1,40 @@
+package org.hansung.roadbuddy.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hansung.roadbuddy.dto.naver.request.PlaceSearchReqDto;
+import org.hansung.roadbuddy.dto.naver.response.PlaceSearchResDto;
+import org.hansung.roadbuddy.enums.HttpMethods;
+import org.hansung.roadbuddy.generic.GenericAPIService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.net.http.HttpRequest;
+
+@Service
+public class NaverOpenAPIService extends GenericAPIService {
+    private final String clientId;
+    private final String secretKey;
+    private final String placeSearchEndpoint = "https://openapi.naver.com/v1/search/local.json";
+    protected NaverOpenAPIService(ObjectMapper objectMapper,
+                                  @Value("${api.key.naver.open.clientId}") String clientId,
+                                  @Value("${api.key.naver.open.secret}") String secretKey) {
+        super(objectMapper, null);
+        this.clientId = clientId;
+        this.secretKey = secretKey;
+    }
+
+    @Override
+    protected HttpRequest.Builder createHttpRequestBuilder() {
+        return HttpRequest.newBuilder()
+                .header("X-Naver-Client-Id", clientId)
+                .header("X-Naver-Client-Secret", secretKey);
+    }
+
+    public PlaceSearchResDto getPlaceSearch(PlaceSearchReqDto placeSearchReqDto) throws JsonProcessingException {
+        String response = sendRequest(placeSearchEndpoint, HttpMethods.GET, createHttpRequestBuilder(), placeSearchReqDto);
+        PlaceSearchResDto placeSearchResDto = objectMapper.readValue(response, PlaceSearchResDto.class);
+        placeSearchResDto.setCoordinate(placeSearchReqDto.getCoordinate());
+        return placeSearchResDto;
+    }
+}
