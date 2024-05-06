70d50320bb2dae4f6ae5bfbba61109dfd58e9058
S-DPR <thankyouformuch@naver.com>
2024-05-06 22:24:09 +0900

    feat: scrf 비활성화

---
+++ src/main/java/org/hansung/roadbuddy/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package org.hansung.roadbuddy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll())
+                .httpBasic(httpBasic -> {});
+        return http.build();
+    }
+}
