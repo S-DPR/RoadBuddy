19c1e098bc9dae3956aeccb2ff09d9729b0126d2
S-DPR <thankyouformuch@naver.com>
2024-05-13 00:39:22 +0900

    refactoring: DtoGetUtils 활용

--- src/main/java/org/hansung/roadbuddy/service/SteepSlopeService.java
+++ src/main/java/org/hansung/roadbuddy/service/SteepSlopeService.java
@@ -4,6 +4,7 @@ import org.hansung.roadbuddy.dto.google.response.googleDirections.Legs;
 import org.hansung.roadbuddy.dto.google.response.googleDirections.Steps;
 import org.hansung.roadbuddy.dto.steepSlope.SteepSlope;
 import org.hansung.roadbuddy.dto.steepSlope.SteepSlopePool;
+import org.hansung.roadbuddy.utilService.DtoGetUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -19,10 +20,11 @@ public class SteepSlopeService {
 
     public Legs updateSteepSlopes(Legs legs) {
         legs.getSteps().forEach(step -> {
-            if (isSubway(step)) {
+            if (DtoGetUtils.isSubway(step)) {
                 step.setSteep_slope(new ArrayList<>());
                 step.getSteep_slope().addAll(getDepartureSteepSlopeBySubway(step));
                 step.getSteep_slope().addAll(getArrivalSteepSlopeBySubway(step));
+                step.setSteep_slope(step.getSteep_slope().stream().distinct().toList());
             }
         });
         return legs;
@@ -30,21 +32,14 @@ public class SteepSlopeService {
 
     // line과 station에 해당하는 가파른 경사 데이터를 반환하는 메서드
     public List<SteepSlope> getDepartureSteepSlopeBySubway(Steps steps) {
-        String line = steps.getTransit_details().getLine().getShort_name();
-        String station = (String) steps.getTransit_details().getDeparture_stop().get("name");
-        System.out.println("line station = " + line + " " + station);
+        String line = DtoGetUtils.getLineShortName(steps);
+        String station = DtoGetUtils.getDepartureStop(steps);
         return steepSlopePool.get(line, station);
     }
 
     public List<SteepSlope> getArrivalSteepSlopeBySubway(Steps steps) {
-        String line = steps.getTransit_details().getLine().getShort_name();
-        String station = (String) steps.getTransit_details().getArrival_stop().get("name");
-        System.out.println("line station = " + line + " " + station);
+        String line = DtoGetUtils.getLineShortName(steps);
+        String station = DtoGetUtils.getArrivalStop(steps);
         return steepSlopePool.get(line, station);
-    }
-
-    private boolean isSubway(Steps step) {
-        if (!step.getTravel_mode().equals("TRANSIT")) return false;
-        return step.getTransit_details().getLine().getVehicle().getType().equals("SUBWAY");
     }
 }
