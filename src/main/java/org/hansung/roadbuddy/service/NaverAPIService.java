f4bb37b88d82eeba31ec44b5252f0f14eb9c9b4d
S-DPR <thankyouformuch@naver.com>
2024-05-13 10:34:02 +0900

    refactoring: 메소드 분할

--- src/main/java/org/hansung/roadbuddy/service/NaverAPIService.java
+++ src/main/java/org/hansung/roadbuddy/service/NaverAPIService.java
@@ -3,8 +3,6 @@ package org.hansung.roadbuddy.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hansung.roadbuddy.dto.naver.request.NaverGeocodingReqDto;
-import org.hansung.roadbuddy.dto.naver.request.PlaceSearchItemReqDto;
-import org.hansung.roadbuddy.dto.naver.request.PlaceSearchReqDto;
 import org.hansung.roadbuddy.dto.naver.response.PlaceSearchItem;
 import org.hansung.roadbuddy.dto.naver.response.PlaceSearchResDto;
 import org.hansung.roadbuddy.enums.HttpMethods;
@@ -13,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.net.http.HttpRequest;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -39,25 +38,36 @@ public class NaverAPIService extends GenericAPIService {
 
     public Map getGeocoding(NaverGeocodingReqDto naverGeocodingReqDto) throws JsonProcessingException {
         if (cache.containsKey(naverGeocodingReqDto)) return (Map) cache.get(naverGeocodingReqDto);
-        List<String> words = Arrays.stream(naverGeocodingReqDto.getQuery().split(" ")).toList();
-        for (int len = words.size(); len >= 1; len--) {
-            for (int start = words.size()-len; start >= 0; start--) {
-                String roadAddress = String.join(" ", words.subList(start, start + len));
-                naverGeocodingReqDto.setQuery(roadAddress);
-                try {
-                    String response = sendRequest(geocodingEndpoint, HttpMethods.GET, createHttpRequestBuilder(), naverGeocodingReqDto);
-                    Map ret = objectMapper.readValue(response, Map.class);
-//                    System.out.println("((List)res.get(\"addresses\")).size()) = " + ((List)ret.get("addresses")).size());
-                    if (((List)ret.get("addresses")).size() == 0) continue;
-                    naverGeocodingReqDto.setQuery(String.join(" ", words));
-                    cache.putIfAbsent(naverGeocodingReqDto, ret);
-                    return ret;
-                } catch (JsonProcessingException e) {
-                    throw new RuntimeException(e);
-                }
+        // 만약 주어진 주소가 A B C D E라면,
+        // 'A B C D E'를 모두 쿼리에 넣으면 결과가 나오지 않지만
+        // 'C D E'처럼 일부만 넣을 경우 결과가 나오는 경우가 있다.
+        // 이를 해결하기 위해 길이를 기본으로 가능한 모든 방법을 넣기로 한다.
+        String originQuery = naverGeocodingReqDto.getQuery();
+        List<String> combination = getAddressCombination(originQuery);
+        for (String query: combination) {
+            try {
+                naverGeocodingReqDto.setQuery(query);
+                String response = sendRequest(geocodingEndpoint, HttpMethods.GET, createHttpRequestBuilder(), naverGeocodingReqDto);
+                Map ret = objectMapper.readValue(response, Map.class);
+                naverGeocodingReqDto.setQuery(originQuery);
+                cache.putIfAbsent(naverGeocodingReqDto, ret);
+                return ret;
+            } catch (JsonProcessingException ignored) {
             }
         }
         return null;
+    }
+
+    private List<String> getAddressCombination(String query) {
+        List<String> ret = new ArrayList<>();
+        List<String> words = Arrays.stream(query.split(" ")).toList();
+        for (int len = words.size(); len >= 1; len--) {
+            for (int start = words.size()-len; start >= 0; start--) {
+                String roadAddress = String.join(" ", words.subList(start, start + len));
+                if (!roadAddress.isEmpty()) ret.add(roadAddress);
+            }
+        }
+        return ret;
     }
 
     public PlaceSearchResDto updatePlaceSearchItemsGeocoding(PlaceSearchResDto placeSearchResDto) {
