a911d013c7ff74bcf98df7bc9adc4688c911bacb
S-DPR <thankyouformuch@naver.com>
2024-05-18 17:45:07 +0900

    feat: Subway에다가 numStop이 null인 경우 직접 찾아서 넣어주는 로직 추가

--- src/main/java/org/hansung/roadbuddy/service/GoogleAPIService.java
+++ src/main/java/org/hansung/roadbuddy/service/GoogleAPIService.java
@@ -8,6 +8,8 @@ import org.hansung.roadbuddy.dto.google.request.GeocodingReqDto;
 import org.hansung.roadbuddy.dto.google.request.TextSearchReqDto;
 import org.hansung.roadbuddy.dto.google.response.googleDirections.GoogleDirectionResDto;
 import org.hansung.roadbuddy.dto.google.response.googleDirections.Routes;
+import org.hansung.roadbuddy.dto.utils.SubwayInfo;
+import org.hansung.roadbuddy.dto.utils.SubwayInfoPool;
 import org.hansung.roadbuddy.generic.GenericAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,16 +19,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import static org.hansung.roadbuddy.utilService.DtoGetUtils.isSubway;
+
 @Service
 public class GoogleAPIService extends GenericAPIService {
     private final String geoCodingEndpoint = "https://maps.googleapis.com/maps/api/geocode/json";
     private final String googlePlaceEndpoint = "https://maps.googleapis.com/maps/api/place/autocomplete/json";
     private final String googleDirectionsEndpoint = "https://maps.googleapis.com/maps/api/directions/json";
     private final String googleTextSearch = "https://maps.googleapis.com/maps/api/place/textsearch/json";
+    private final SubwayInfoPool subwayInfoPool;
 
     @Autowired
-    GoogleAPIService(ObjectMapper objectMapper, @Value("${api.key.google}") String apiKey) {
+    GoogleAPIService(ObjectMapper objectMapper, @Value("${api.key.google}") String apiKey, SubwayInfoPool subwayInfoPool) {
         super(objectMapper, apiKey);
+        this.subwayInfoPool = subwayInfoPool;
     }
 
     public Map getAddressCoordinates(GeocodingReqDto geocodingReqDto) throws JsonProcessingException {
@@ -52,6 +58,7 @@ public class GoogleAPIService extends GenericAPIService {
         String response = sendRequest(googleDirectionsEndpoint, directionReqDto);
         GoogleDirectionResDto googleDirectionResDto = objectMapper.readValue(response, GoogleDirectionResDto.class);
         delTrain(googleDirectionResDto);
+        updateNullNumStops(googleDirectionResDto);
         return googleDirectionResDto;
     }
 
@@ -71,5 +78,20 @@ public class GoogleAPIService extends GenericAPIService {
             if (!hasTrain) routes.add(route);
         });
         googleDirectionResDto.setRoutes(routes);
+    }
+
+    private void updateNullNumStops(GoogleDirectionResDto googleDirectionResDto) {
+        googleDirectionResDto.getRoutes().forEach(route -> {
+            route.getLegs().forEach(leg -> {
+                leg.getSteps().forEach(step -> {
+                    if (isSubway(step) && step.getTransit_details().getNum_stops() == null) {
+                        SubwayInfo start = subwayInfoPool.getByStepWithDeparture(step);
+                        SubwayInfo end = subwayInfoPool.getByStepWithArrival(step);
+                        step.getTransit_details().setNum_stops(start.findStationShortestDistance(end)+"");
+                        System.out.println("step.getTransit_details().getNum_stops() = " + step.getTransit_details().getNum_stops());
+                    }
+                });
+            });
+        });
     }
 }
